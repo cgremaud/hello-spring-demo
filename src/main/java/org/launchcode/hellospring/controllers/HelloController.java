68a3c619d993ef415f11b18c4e7526e543ff19ce
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-18 10:57:34 -0500

    finished dynamic views section

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -21,16 +21,18 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting); //attributeName matches up with the name used in the template.
+        return "hello";
     }
 
    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // /hello/form
@@ -39,4 +41,25 @@ public class HelloController {
         return "form";
     }
 
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Lex Luthor");
+        names.add("Superman");
+        names.add("Batgirl");
+        model.addAttribute("names", names);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(-5);
+        numbers.add(0);
+        numbers.add(3);
+        numbers.add(8);
+        numbers.add(12);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
+
+
+        return "hello-list";
+    }
 }
